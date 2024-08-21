27a0c5f473f737bc09c16a26ccd2fa09fc39c7fe
Badr Madiani <33285426+madianibadr@users.noreply.github.com>
2024-08-20 22:19:09 -0400

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,34 @@
 # run on tor network
-#c.content.proxy = 'socks5://localhost:9050/'
+# c.content.proxy = 'socks5://localhost:9050/'
 
-###calling userscripts
+# config.bind('<Ctrl-h>', 'fake-key <Backspace>', 'insert')
+# Opens vim to edit current text
+config.bind('<Ctrl+e>', 'edit-text')
+
+# Enable private browsing mode to reduce cache usage
+c.content.private_browsing = False
+
+# Disable auto-playing of videos
+c.content.autoplay = False
+
+# Limit the number of open tabs
+c.tabs.last_close = 'close'
+
+### appearance
+
+# hide tabs when only one tab
+c.tabs.show = 'multiple'
+
+#hide status bar
+c.statusbar.show = 'in-mode'
+
+#disable webgl
+c.content.webgl = False
+
+#limit history
+c.history_gap_interval = 10
+
+### calling userscripts
 #yt_download
 config.bind('<Alt-d>', 'spawn --userscript yt_download')
 
@@ -25,99 +52,23 @@ config.bind('ww', 'open -t https://www.w3schools.com/python')
 config.bind('xx', 'open -t https://web.whatsapp.com')
 config.bind('wc', 'open -t https://chatgpt.com/')
 
-#config.bind('gi', 'hint images yank')  # Bind 'gi' to yank image URL
-#config.bind('go', 'spawn --detach feh {clipboard}')  # Bind 'go' to open URL with feh
+#spawn photos feh
 config.bind('go', 'hint images run :spawn feh {hint-url}')
-config.bind('gv', 'hint links spawn mpv {hint-url}')
-#config.bind('gV', 'hint links spawn --detach mpv --ytdl-raw-options=path=/home/sumida/%(title)s.%(ext)s {hint-url}')
-config.bind('gV', 'hint links spawn --detach yt-dlp -o "/home/sumida/%(title)s.%(ext)s" {hint-url}')
+
+# config.bind('gv', 'hint links spawn mpv {hint-url}')
+config.bind('gv', 'hint links spawn mpv --ytdl-format="bestvideo[height<=720]+bestaudio/best[height<=720]" {hint-url}')
 
 # Change the argument to True to still load settings configured via autoconfig.yml
 config.load_autoconfig(False)
 
-# Which cookies to accept. With QtWebEngine, this setting also controls
-# other features with tracking capabilities similar to those of cookies;
-# including IndexedDB, DOM storage, filesystem API, service workers, and
-# AppCache. Note that with QtWebKit, only `all` and `never` are
-# supported as per-domain values. Setting `no-3rdparty` or `no-
-# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
-# `all`. If this setting is used with URL patterns, the pattern gets
-# applied to the origin/first party URL of the page making the request,
-# not the request URL. With QtWebEngine 5.15.0+, paths will be stripped
-# from URLs, so URL patterns using paths will not match. With
-# QtWebEngine 5.15.2+, subdomains are additionally stripped as well, so
-# you will typically need to set this setting for `example.com` when the
-# cookie is set on `somesubdomain.example.com` for it to work properly.
-# To debug issues with this setting, start qutebrowser with `--debug
-# --logfilter network --debug-flag log-cookies` which will show all
-# cookies being set.
-# Type: String
-# Valid values:
-#   - all: Accept all cookies.
-#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
-#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
-#   - never: Don't accept cookies at all.
-config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'never', 'chrome-devtools://*')
 
-# Which cookies to accept. With QtWebEngine, this setting also controls
-# other features with tracking capabilities similar to those of cookies;
-# including IndexedDB, DOM storage, filesystem API, service workers, and
-# AppCache. Note that with QtWebKit, only `all` and `never` are
-# supported as per-domain values. Setting `no-3rdparty` or `no-
-# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
-# `all`. If this setting is used with URL patterns, the pattern gets
-# applied to the origin/first party URL of the page making the request,
-# not the request URL. With QtWebEngine 5.15.0+, paths will be stripped
-# from URLs, so URL patterns using paths will not match. With
-# QtWebEngine 5.15.2+, subdomains are additionally stripped as well, so
-# you will typically need to set this setting for `example.com` when the
-# cookie is set on `somesubdomain.example.com` for it to work properly.
-# To debug issues with this setting, start qutebrowser with `--debug
-# --logfilter network --debug-flag log-cookies` which will show all
-# cookies being set.
-# Type: String
-# Valid values:
-#   - all: Accept all cookies.
-#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
-#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
-#   - never: Don't accept cookies at all.
-config.set('content.cookies.accept', 'all', 'devtools://*')
+config.set('content.cookies.accept', 'never', 'devtools://*')
 
-# Value to send in the `Accept-Language` header. Note that the value
-# read from JavaScript is always the global value.
-# Type: String
 config.set('content.headers.accept_language', '', 'https://matchmaker.krunker.io/*')
 
-# User agent to send.  The following placeholders are defined:  *
-# `{os_info}`: Something like "X11; Linux x86_64". * `{webkit_version}`:
-# The underlying WebKit version (set to a fixed value   with
-# QtWebEngine). * `{qt_key}`: "Qt" for QtWebKit, "QtWebEngine" for
-# QtWebEngine. * `{qt_version}`: The underlying Qt version. *
-# `{upstream_browser_key}`: "Version" for QtWebKit, "Chrome" for
-# QtWebEngine. * `{upstream_browser_version}`: The corresponding
-# Safari/Chrome version. * `{qutebrowser_version}`: The currently
-# running qutebrowser version.  The default value is equal to the
-# unchanged user agent of QtWebKit/QtWebEngine.  Note that the value
-# read from JavaScript is always the global value. With QtWebEngine
-# between 5.12 and 5.14 (inclusive), changing the value exposed to
-# JavaScript requires a restart.
-# Type: FormatString
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
 
-# User agent to send.  The following placeholders are defined:  *
-# `{os_info}`: Something like "X11; Linux x86_64". * `{webkit_version}`:
-# The underlying WebKit version (set to a fixed value   with
-# QtWebEngine). * `{qt_key}`: "Qt" for QtWebKit, "QtWebEngine" for
-# QtWebEngine. * `{qt_version}`: The underlying Qt version. *
-# `{upstream_browser_key}`: "Version" for QtWebKit, "Chrome" for
-# QtWebEngine. * `{upstream_browser_version}`: The corresponding
-# Safari/Chrome version. * `{qutebrowser_version}`: The currently
-# running qutebrowser version.  The default value is equal to the
-# unchanged user agent of QtWebKit/QtWebEngine.  Note that the value
-# read from JavaScript is always the global value. With QtWebEngine
-# between 5.12 and 5.14 (inclusive), changing the value exposed to
-# JavaScript requires a restart.
-# Type: FormatString
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0', 'https://accounts.google.com/*')
 
 # Load images automatically in web pages.
@@ -169,23 +120,17 @@ config.set('content.notifications.enabled', False, 'https://www.facebook.com')
 #   - ask
 config.set('content.register_protocol_handler', False, 'https://mail.google.com?extsrc=mailto&url=%25s')
 
-
-
 c.url.searchengines = {
     'DEFAULT':  'https://duckduckgo.com/?ia=web&q={}',
+    'gm':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
+    'g':      'https://google.com/search?q={}',
+    'm':       'https://www.google.com/maps/search/{}',
     'dd':      'https://thefreedictionary.com/{}',
     'e':       'https://www.ebay.com/sch/i.html?_nkw={}',
     'gh':      'https://github.com/search?o=desc&q={}&s=stars',
     '!gist':    'https://gist.github.com/search?q={}',
-    '!gi':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
-    'g':      'https://google.com/search?q={}',
-    '!ig':      'https://www.instagram.com/explore/tags/{}',
-    'm':       'https://www.google.com/maps/search/{}',
     '!p':       'https://pry.sh/{}',
     '!r':       'https://www.reddit.com/search?q={}',
-    '!sd':      'https://slickdeals.net/newsearch.php?q={}&searcharea=deals&searchin=first',
-    '!t':       'https://www.thesaurus.com/browse/{}',
     '!w':       'https://en.wikipedia.org/wiki/{}',
-    '!yelp':    'https://www.yelp.com/search?find_desc={}',
     'yt':      'https://www.youtube.com/results?search_query={}'
 }
